a17ec17304d94b23f19a520ef8341e3139a0d3ed
khanhlam90 <91096296+khanhlam90@users.noreply.github.com>
2022-01-07 13:20:18 -0800

    verify pw with compareSync

--- models/User.js
+++ models/User.js
@@ -4,7 +4,12 @@ const bcrypt = require('bcrypt');
 
 // create our User model - This Model class is what we create our own models 
 // from using the extends keyword so User inherits all of the functionality the Model class has.
-class User extends Model {}
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 // define table columns and configuration -
 // initialize the model's data and configuration, passing in two objects as arguments
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -57,6 +57,33 @@ router.post('/', (req, res) => {
     });
 });
 
+// this is for LOGIN
+router.post('/login', (req, res) => {
+  // expects {email: 'lernantino@gmail.com', password: 'password1234'}
+    User.findOne({
+      where: {
+        email: req.body.email
+        }
+      }).then(dbUserData => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'No user with that email address!' });
+        return;
+      }
+
+      // add comment syntax in front of this line in the .then()
+      // res.json({ user: dbUserData });
+  
+      // Verify user
+      const validPassword = dbUserData.checkPassword(req.body.password);
+
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });  
+});
+
 // PUT /api/users/1
 // UPDATE users
 // SET username = "Lernantino", email = "lernantino@gmail.com", password = "newPassword1234"
