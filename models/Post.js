8e4d41680d2bf88d276ad5929a97173a14f1c8fe
khanhlam90 <91096296+khanhlam90@users.noreply.github.com>
2022-01-07 16:26:15 -0800

    Post table created

---
+++ models/Post.js
@@ -0,0 +1,49 @@
+//import the elements that we'll need to build the Post model
+const { Model, DataTypes } = require('sequelize'); // Model and Datatypes we'll use from the sequelize package.
+const sequelize = require('../config/connection'); //the connection to MySQL we stored in the connection.js file
+
+// create our Post model
+class Post extends Model {}
+
+// create fields/columns for Post model
+Post.init(
+    // first parameters - define Post schema
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        post_url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+            isURL: true
+        }
+    },
+        // this column determines who posted the news article    
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: { //references property establish the relationship between this post and the user by creating a reference to the User model
+                model: 'user',
+                key: 'id' //specifically to the id column that is defined by the key property, which is the primary key
+            }
+        }
+    },
+    
+    // second parameter - we configure the metadata, including the naming conventions.
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+// export expression to make the Post model accessible to other parts of the application
+module.exports = Post;
