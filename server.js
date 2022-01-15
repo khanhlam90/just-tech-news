cab16f03c0598cf4ffa15428c4333ee029a33bf0
khanhlam90 <91096296+khanhlam90@users.noreply.github.com>
2022-01-15 00:11:59 -0800

    Create a homepage with Handlebars.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers/');
+const path = require('path');
 
 //importing the connection to Sequelize from config/connection.js
 const sequelize = require('./config/connection');
@@ -7,8 +8,16 @@ const sequelize = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// set up Handlebars.js
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
